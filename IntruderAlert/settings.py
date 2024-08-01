5b77b0a161b24ca0d39995f45735d0eeba7e1f68
Andrew Reyna <1711ajreyna@gmail.com>
2024-08-01 15:55:51 -0700

    update secret key management

--- IntruderAlert/settings.py
+++ IntruderAlert/settings.py
@@ -74,17 +74,11 @@ WSGI_APPLICATION = 'IntruderAlert.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # Heroku enviorment check
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES = {
+        'default': dj_database_url.config(default='sqlite:///db.sqlite3')
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
