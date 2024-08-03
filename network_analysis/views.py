053f962ece45dc86ea1b5830f4d2b4b5c62dcd52
Andrew Reyna <1711ajreyna@gmail.com>
2024-08-02 18:22:38 -0700

    presentation version

--- network_analysis/views.py
+++ network_analysis/views.py
@@ -30,22 +30,16 @@ def process_pcap(filepath):
                     length=packet.length
                 )
             except AttributeError as e:
-                print("\n\n\n\n\n")
-                print("***error 2 ")
                 print(e)
-                print("\n\n\n\n\n")
                 continue
     except Exception as e:
-        print("\n\n\n\n\n")
-        print("***error 1 ")
         print(e)
-        print("\n\n\n\n\n")
         raise e
-    finally:
-        asyncio.gather(
-            capture.close_async(),
-            capture._get_tshark_process()
-        )
+    # finally:
+    #     asyncio.gather(
+    #         capture.close_async(),
+    #         capture._get_tshark_process()
+    #     )
 
         
 def analyze_pcap(request):
@@ -54,32 +48,17 @@ def analyze_pcap(request):
         fs = FileSystemStorage()
         filename = fs.save(pcap_file.name, pcap_file)
         filepath = fs.path(filename)
-        print("\n\n\n\n")
-        print(f"fileName {filename}")
-        print(f"filepath {filepath}")
-        print("\n\n\n\n")
 
         
         try:
             process_pcap(filepath)  # Await the async function directly
-            print("\n\n\n\n")
-            print(f"after process pcap")
-            print("\n\n\n\n")
 
 
             # Fetch network traffic data after processing
             traffic = NetworkTraffic.objects.all()
-            print("\n\n\n\n")
-            print(f"traffic")
-            print(traffic)
-            print("\n\n\n\n")
             return render(request, 'analyze_pcap.html', {'traffic': traffic})
 
         except Exception as e:
-            print("\n\n\n\n")
-            print(f"main exception")
-            print(e)
-            print("\n\n\n\n")
             return HttpResponse(f"Error processing pcap: {str(e)}", status=500)
     
     return render(request, 'analyze_pcap.html')
