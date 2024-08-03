ad8ebfc3d475040c39468851ac267ffb260df2b3
Andrew Reyna <1711ajreyna@gmail.com>
2024-08-02 17:57:18 -0700

    more error handeling

--- network_analysis/views.py
+++ network_analysis/views.py
@@ -22,6 +22,7 @@ async def process_pcap(filepath):
         capture = pyshark.FileCapture(filepath)
         for packet in capture:
             try:
+                print("*** line in for")
                 NetworkTraffic.objects.create(
                     src_ip=packet.ip.src,
                     dst_ip=packet.ip.dst,
@@ -30,11 +31,15 @@ async def process_pcap(filepath):
                 )
             except AttributeError as e:
                 print("\n\n\n\n\n")
-                print("***error ")
+                print("***error 2 ")
                 print(e)
                 print("\n\n\n\n\n")
                 continue
     except Exception as e:
+        print("\n\n\n\n\n")
+        print("***error 1 ")
+        print(e)
+        print("\n\n\n\n\n")
         raise e
     finally:
         await asyncio.gather(
@@ -57,12 +62,24 @@ def analyze_pcap(request):
         
         try:
             process_pcap(filepath)  # Await the async function directly
+            print("\n\n\n\n")
+            print(f"after process pcap")
+            print("\n\n\n\n")
+
 
             # Fetch network traffic data after processing
             traffic = NetworkTraffic.objects.all()
+            print("\n\n\n\n")
+            print(f"traffic")
+            print(traffic)
+            print("\n\n\n\n")
             return render(request, 'analyze_pcap.html', {'traffic': traffic})
 
         except Exception as e:
+            print("\n\n\n\n")
+            print(f"main exception")
+            print(e)
+            print("\n\n\n\n")
             return HttpResponse(f"Error processing pcap: {str(e)}", status=500)
     
     return render(request, 'analyze_pcap.html')
